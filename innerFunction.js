26af9bf7962b30e820d7de5c2fcd7aa156593c0c
mustafa-arkan <arkancse@gmail.com>
2022-08-06 18:11:15 +0600

    function added

---
+++ innerFunction.js
@@ -0,0 +1,57 @@
+// function greeting(greet, name){
+
+//    function getFirstName(){
+//      if(name){
+
+//             return name.split(' ')[0]
+
+//      }
+
+
+//    }    //end of inner function          node js-program-solve/innerFunction
+
+
+//    var message=(greet +' '+getFirstName())
+//    console.log(message)
+
+
+// }   //end of outer function
+
+
+// greeting('Good Morning', 'Mustafa Arkan')
+
+
+  function greeting(greet,name){
+
+
+ function getFirstName(){
+
+   if(name){
+
+
+    return name.split(' ')[0]
+   }
+
+
+
+
+
+
+ }   //end of inner function
+
+
+
+
+    var message=(greet + ' ' + getFirstName())
+console.log(message)
+
+
+
+
+
+
+
+
+  }    //end of outer function
+
+greeting('Good Morning Mr.','Arkan Mustafa')
---
+++ practice.js
@@ -0,0 +1,198 @@
+// 1) Write a function called foo() which prints “foo” and a function called bar() which prints
+// “bar”. Call function bar() in the foo() function after printing. What will be the output? Now call
+// the foo() to see the output.
+
+// 2) Write a function called make_avg() which will take an three integers and return the
+// average of those values.
+
+// Practise Task
+
+// 3) Challenging: Write a function called make_avg() which will take an array of integers and
+// the size of that array and return the average of those values.
+
+// 4) Write a function called odd_even() which takes an integer value and tells whether this
+// value is even or odd. You need to do it in 4 ways:
+// ● Has return + Has parameter
+// ● No return + Has parameter
+
+// Practise Task
+
+// 5) You are in a hurry to go to your school on time. But when you are crossing the road, the
+// traffic signal is red coloured. In this situation, if you try to cross the road, you may be in
+// danger.If you notice a yellow coloured traffic signal, you should stop. If you notice a green
+// coloured traffic signal, you should cross the road. So write a JS code, where there is a
+// variable called signal. Its value can be green, yellow or red & we will get different activities as
+// output depending on the variable. So, hurry up.
+
+
+//1) Write a function called foo() which prints “foo” and a function called bar() which prints
+// “bar”. Call function bar() in the foo() function after printing. What will be the output? Now call
+// the foo() to see the output.   node js-program-solve/practice
+
+
+
+// function foo(){
+//     var foo='foo'
+    
+//     console.log(foo)
+    
+// }
+// foo()
+
+
+// function bar(){
+//     var bar='bar'
+    
+//   console.log(bar)  
+// }
+// bar()
+
+
+
+
+
+
+
+// function foo(){
+    
+//     bar()
+//     var foo='foo'
+
+   
+// console.log(foo,bar())
+   
+   
+    
+// }
+
+// function bar(){
+//     var bar='bar'
+    
+    
+// }
+
+// foo()
+
+
+
+
+// // hsfkrh
+
+// function function_one() {
+//     function_two(); // considering the next alert, I figured you wanted to call function_two first
+//     console.log("The function called 'function_one' has been called.");
+// }
+
+// function function_two() {
+//     console.log("The function called 'function_two' has been called.");
+// }
+
+// function_one();
+
+
+
+// function foo(foo,bar){
+//     function bar(bar){
+
+
+//         console.log(foo,bar)
+//     }
+
+
+// bar()
+
+
+// }
+
+
+// foo(foo)
+
+
+
+
+
+
+
+
+// function bar(){
+//     let br='bar'
+//     return br
+        
+//     }
+    
+//     let he=bar()
+//     console.log(he)
+// // end of bar()
+
+// function foo(){
+
+//     let fb='foo'
+//     console.log(he,fb)
+
+// return fb
+    
+// }
+
+// let me=foo()
+// console.log(me)
+
+// ********************
+// var foobar=function(){
+// console.log('foo')
+// return
+
+// }
+
+
+// var another=foobar
+// console.log(another)
+
+//**************** */
+
+
+// var add=function(a,b){
+
+
+//     return a+b
+// }
+// // var another=add
+// // console.log(another(10,40))
+
+// add(10,50)
+
+
+var foobar2=function(){
+
+
+    // console.log('foo')
+    return 'bar'
+}
+
+var programming=foobar2()
+console.log(programming)
+
+
+
+
+
+
+
+
+
+
+
+var foobar=function(){
+
+
+     console.log(foobar2)
+    return 
+
+'foo'
+
+
+
+    
+}
+
+var hero=foobar()
+console.log(hero)
